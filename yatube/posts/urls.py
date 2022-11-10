ab38fb8b31e260690f8385c12be9c21907b0e0e5
Denis Larionov <bullet_proof87@mail.ru>
2022-11-10 20:00:33 +0300

    коментарий

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = 'posts'
-
+#коментарий
 urlpatterns = [
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
     path('create/', views.post_create, name='post_create'),
